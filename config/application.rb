aeaf762cfab9738df2a5e4e1a55f405e07791e86
Apple <Apple@MacBook-Pro-Apple.local>
2022-12-03 18:33:55 +0300

    lesson3.2

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,9 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.I18n.default_locale = :ru
+    config.i18n.default_locale = :ru
     config.time_zone = "Moscow"
-    
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
---
+++ db/migrate/20221124143559_create_tests.rb
@@ -0,0 +1,12 @@
+class CreateTests < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tests do |t|
+      t.string :title, null: false
+      t.integer :level, default: 0
+      t.references :user_id, user: true, null: false, foreign_key: true
+      t.references :category_id, category: true, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
